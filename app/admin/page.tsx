99592991f3ab0feaf2271a11e53b42aa8b82a4d7
TSCLLP <rajesh@boujeeprojects.com>
2025-12-04 19:10:50 +0530

    Remove PricingSection usage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -215,7 +215,6 @@ export default function HomePage() {
       </section>
 
       {/* Pricing Section - Interactive */}
-      <PricingSection />
 
       {/* Testimonials */}
       <Testimonials />
